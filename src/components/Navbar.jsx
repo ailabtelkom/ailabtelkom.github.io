de7ed98fab0c5b135e5cd3ab413aeed4633194f2
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2020-09-20 01:54:06 +0700

    add navbar drawer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,113 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import {
   Box,
   IconButton,
   useColorMode,
   useColorModeValue,
+  Stack,
+  Text,
 } from "@chakra-ui/core";
-import { FaSun, FaMoon } from "react-icons/fa";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from "@chakra-ui/core";
+import { FaSun, FaMoon, FaBars } from "react-icons/fa";
+import Link from "./Link";
 
 const Navbar = () => {
   const { toggleColorMode } = useColorMode();
   const Icons = useColorModeValue(<FaSun />, <FaMoon />);
+  const color = useColorModeValue("gray.800", "white");
+  const bgColor = useColorModeValue("white", "#1A202C");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+  const [isTop, setIsTop] = useState(true);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const top = window.scrollY < 20;
+      if (top !== isTop) {
+        setIsTop(top);
+      }
+    });
+  }, []);
   return (
     <Box
+      style={{ background: isTop ? "transparent" : bgColor }}
       w="full"
       position="fixed"
       top
-      zIndex={1000}
-      p={1}
-      px={{ default: 1, lg: 5 }}
-      textAlign="right"
+      zIndex={1}
+      p={3}
+      px={{ default: 4, lg: 5 }}
+      d="flex"
+      alignItems="center"
+      justifyContent="space-between"
     >
-      <IconButton
-        variant="transparent"
-        size="sm"
-        onClick={toggleColorMode}
-        icon={Icons}
-      />
+      <Stack direction="row" spacing={5}>
+        <Link route href="/" fontWeight="bold" color={color}>
+          #AmazingAI
+        </Link>
+        <Stack direction="row" d={{ base: "none", lg: "flex" }}>
+          <Link route href="/playground" color={color}>
+            Playground
+          </Link>
+        </Stack>
+      </Stack>
+      <Stack direction="row">
+        <IconButton
+          variant="transparent"
+          size="sm"
+          onClick={toggleColorMode}
+          icon={Icons}
+        />
+        <IconButton
+          d={{ base: "flex", lg: "none" }}
+          variant="transparent"
+          size="sm"
+          onClick={onOpen}
+          icon={<FaBars />}
+          ref={btnRef}
+        />
+      </Stack>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay zIndex={999}>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>AI Laboratory</DrawerHeader>
+
+            <DrawerBody>
+              <Stack>
+                <Link route href="/playground" color={color}>
+                  Playground
+                </Link>
+                <Link route href="/" color={color}>
+                  About
+                </Link>
+              </Stack>
+            </DrawerBody>
+
+            <DrawerFooter>
+              <Text fontSize="sm">
+                Developed by{" "}
+                <Link isExternal href="https://wisesa.dev">
+                  @raisoturu
+                </Link>
+              </Text>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
     </Box>
   );
 };
