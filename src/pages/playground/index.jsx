e92aca13e8ceaba15ee990463cf002c62c8c7dd8
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2020-09-21 20:49:13 +0700

    add monalisa effect and eliza

--- src/pages/playground/index.jsx
+++ src/pages/playground/index.jsx
@@ -42,6 +42,15 @@ const index = () => {
       isExternal: false,
     },
     {
+      name: "Monalisa Effect",
+      url: "https://monalisaeffect.com/",
+      detail: "https://github.com/emilyxxie/mona_lisa_eyes",
+      image:
+        "https://github.com/emilyxxie/mona_lisa_eyes/raw/master/demo_giphy.gif",
+      desc: `Urban legend says that Mona Lisa's eyes will follow you as you move around the room. This is known as the "Mona Lisa Effect." If you enable your webcam, the eyes of this digital portrait will do just that.`,
+      isExternal: true,
+    },
+    {
       name: "Teachable Machine",
       url: "https://teachablemachine.withgoogle.com/",
       image:
@@ -53,11 +62,20 @@ const index = () => {
       isExternal: true,
     },
     {
+      name: "Eliza Line Chatbot",
+      url: "https://line.me/R/ti/p/%40537amjdd",
+      detail: "https://github.com/raisoturu/eliza-line-chatbot",
+      image:
+        "https://media.graphcms.com/resize=w:450,fit:crop/output=format:webp/compress/4yRG5k1QAyQ7v8CVCDWg",
+      desc:
+        "ELIZA is an early natural language processing computer program created from 1964 to 1966 at the MIT Artificial Intelligence Laboratory by Joseph Weizenbaum.",
+      isExternal: true,
+    },
+    {
       name: "Huggingface Zero-shot classification",
       url: "https://huggingface.co/zero-shot/",
       image: "https://huggingface.co/front/assets/huggingface_logo.svg",
-      desc:
-        "Predict topic of a text whitout training model first using transformers model.",
+      desc: "Predict topic of a text whitout training model.",
       isExternal: true,
     },
     {
