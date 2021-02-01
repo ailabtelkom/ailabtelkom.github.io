32d9a04a4e35f18c5e1b7da2a84ace9ae524834c
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-02-01 11:37:05 +0700

    update for migrate

--- next.config.js
+++ next.config.js
@@ -3,3 +3,32 @@ const optimizedImages = require("next-optimized-images");
 
 const nextConfig = {};
 module.exports = withPlugins([optimizedImages], nextConfig);
+
+// const withOptimizedImages = require('next-optimized-images');
+
+// module.exports = withOptimizedImages({
+//   /* config for next-optimized-images */
+
+//   // your config for other plugins or the general next.js here...
+//   /**
+//    * OPTION B:
+//    * Chained with your own url-loader or file-loader
+//    **/
+//   test: /\.(png|jpe?g)$/,
+//   loaders: [
+//     {
+//       loader: 'lqip-loader',
+//       options: {
+//         base64: true,
+//         palette: false
+//       }
+//     },
+//     {
+//       loader: 'url-loader',
+//       options: {
+//         limit: 8000
+//       }
+//     }
+//   ]
+
+// });
