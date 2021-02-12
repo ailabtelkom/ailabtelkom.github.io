a2bf3402f5cddebd9d6f01cd4611beb07546ecd9
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-02-12 22:38:50 +0700

    add discord redirect

---
+++ pages/discord.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+// Here you would fetch and return the user
+const useUser = () => ({ user: null, loading: false })
+
+export default function Page() {
+  const { user, loading } = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!(user || loading)) {
+      router.push('https://discord.gg/GAQ5SFu495')
+    }
+  }, [user, loading])
+
+  return <p>Redirecting to Discord...</p>
+}
