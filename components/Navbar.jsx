7877bfabf4edbda0790fdc1f52ca514c9e02cfdd
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-02-21 16:05:16 +0700

    update bug index

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
           <Link route href="/about" color={color}>
             About
           </Link>
-          <Link route href="/openmind" color={color} fontWeight="bold" className="blink">
+          <Link route href="/openmind" color={color}>
             Open Mind
           </Link>
           <Link
@@ -149,7 +149,7 @@ const Navbar = () => {
                 <Link p="2" route href="/about" color={color}>
                   About
                 </Link>
-                <Link p="2" route href="/openmind" color={color} fontWeight="bold" className="blink">
+                <Link p="2" route href="/openmind" color={color}>
                   Open Mind
                 </Link>
                 <Link
