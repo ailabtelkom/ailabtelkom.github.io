290f45f30ab22cf0d0afc687cab18a390a54826e
alifnaufalyasin <alifnaufalyasin@gmail.com>
2022-01-19 03:01:26 +0000

    Deploying to production from @ ailabtelkom/ailabtelkom.github.io@ebdae6d0c416827e3ff1eeff02e063172b158315 🚀

--- _next/static/chunks/252f366e-a45cfedf6b4fc8a21752.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,c,n){n.d(c,{ixJ:function(){return a}});var u=n(44405);function a(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"}}]})(t)}}}]);
