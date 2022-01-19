3b56c3c96306567d531fe6530ea974ebfb8a656f
Alif Naufal <alifnaufalyasin@gmail.com>
2022-01-19 09:56:19 +0700

    Update Recruitment Aslab

--- pages/submitAslab.jsx
+++ pages/submitAslab.jsx
@@ -17,7 +17,7 @@ export default function Page() {
 
   return (
     <Heading as="h2" fontSize={{ base: "lg", lg: "xl" }}>
-      Redirecting to Form...
+      Redirecting to Form Recruitment...
     </Heading>
   )
 }
