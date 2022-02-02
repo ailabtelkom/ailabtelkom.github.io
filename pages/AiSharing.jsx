1e8648d0e767d952329422411d11c4ad288b113a
Alif Naufal <alifnaufalyasin@gmail.com>
2022-02-02 15:31:07 +0700

    Update link aisharing

---
+++ pages/AiSharing.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { Heading } from "@chakra-ui/react";
+import { useRouter } from 'next/router'
+
+// Here you would fetch and return the user
+const useUser = () => ({ user: null, loading: false })
+
+export default function Page() {
+  const { user, loading } = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!(user || loading)) {
+      router.push('https://telkomuniversity-ac-id.zoom.us/j/99566436616')
+    }
+  }, [user, loading])
+
+  return (
+    <Heading as="h2" fontSize={{ base: "lg", lg: "xl" }}>
+      Redirecting to Ai Sharing Meeting Room ...
+    </Heading>
+  )
+}
