db0315e277017d8da463536b78824c0e642b4110
Alif Naufal <alifnaufalyasin@gmail.com>
2022-06-21 21:25:34 +0700

    update endpoint getDataMember

--- pages/member.jsx
+++ pages/member.jsx
@@ -133,7 +133,7 @@ const member = (props) => {
 member.getInitialProps = async () => {
   try {
     const res = await Axios.post(
-      "https://botIgadis.aliven.my.id/getSheetAI",
+      "https://be-web-ai.herokuapp.com/getSheetAI",
       {
         id_spreadsheet: "1SaXnuRt-1KXyDGS18KqU7cus4_IWNxpGhiVy0J1vz-U",
         email: "sheet-access@web-amazing-ai.iam.gserviceaccount.com",
