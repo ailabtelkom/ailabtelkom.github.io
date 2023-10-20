2fb289d051452825eaf923d0cc299d003a1b68ba
alifnaufalyasin <alifnaufalyasin@gmail.com>
2023-10-20 22:47:21 +0700

    update

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ module.exports = withPlugins([optimizedImages], nextConfig);
 module.exports = {
   webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+    config.resolve.fallback = { 
+      fs: false,
+      path: false
+    };
 
     return config;
   },
